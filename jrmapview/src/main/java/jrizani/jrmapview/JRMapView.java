63c59623843d315a03e8f5bc4fe469fa58825516
julian noor rizani <juliannoorrizani@gmail.com>
2019-03-11 09:29:43 +0700

    update library and add the javadoc

--- jrmapview/src/main/java/jrizani/jrmapview/JRMapView.java
+++ jrmapview/src/main/java/jrizani/jrmapview/JRMapView.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,24 +28,67 @@ import jrizani.JRMapView.R;
 /*         22 Feb 2019         */
 /*=============================*/
 
+/**
+ * class to create custom View that show mapView and show map type chooser in it
+ */
 public class JRMapView extends ConstraintLayout {
 
+    /**
+     * the real map view in this custom layout
+     */
     private MapView mapView;
+    /**
+     * outer space is view that use to hide map type chooser when it clicked
+     */
     private ImageView outerSpace;
-    private ConstraintLayout layerButton;
+    /**
+     * button to show map type chooser
+     */
+    private ImageButton layerButton;
+    /**
+     * to apply constraint set in this custom layout(use constraintLayout)
+     */
     private ConstraintSet constraintSet;
+    /**
+     * map type chooser view
+     */
     private ConstraintLayout mapChooser;
-
+    /**
+     * callback when map ready
+     */
     private OnMapReadyCallback mOnMapReadyCallback;
+    /**
+     * google map
+     */
     private GoogleMap mGoogleMap;
-
+    /**
+     * button to set map to default type
+     */
     private ImageView defaultMapButton;
+    /**
+     * button to set map to satellite type
+     */
     private ImageView satelliteMapButton;
+    /**
+     * button to set map to terrain button
+     */
     private ImageView terrainMapButton;
+    /**
+     * text under default button
+     */
     private TextView defaultText;
+    /**
+     * text under satellite button
+     */
     private TextView satelliteText;
+    /**
+     * text under terrain button
+     */
     private TextView terrainText;
-    private int topPadding = 56;
+    /**
+     * default top padding of layer button
+     */
+    private int topPadding = 12;
 
     public JRMapView(Context context) {
         super(context);
@@ -66,11 +110,20 @@ public class JRMapView extends ConstraintLayout {
         init();
     }
 
+    /**
+     * method to trigger onCreate of mapView and store the onMapReadyCallback
+     *
+     * @param savedInstanceState is savedInstanceState of activity or fragment
+     * @param onMapReadyCallback is callback implementation
+     */
     public void onCreate(Bundle savedInstanceState, OnMapReadyCallback onMapReadyCallback) {
         mapView.onCreate(savedInstanceState);
         mOnMapReadyCallback = onMapReadyCallback;
     }
 
+    /**
+     * method to trigger mapInitializer and getMapAsync of mapView
+     */
     public void onResume() {
         mapView.onResume();
 
@@ -83,22 +136,37 @@ public class JRMapView extends ConstraintLayout {
         mapView.getMapAsync(mOnMapReadyCallback);
     }
 
+    /**
+     * method to trigger onPause of mapView
+     */
     public void onPause() {
         mapView.onPause();
     }
 
+    /**
+     * method to trigger onStart of mapView
+     */
     public void onStart() {
         mapView.onStart();
     }
 
+    /**
+     * method to trigger onStop of mapView
+     */
     public void onStop() {
         mapView.onStop();
     }
 
+    /**
+     * method to trigger onDestroy of mapView
+     */
     public void onDestroy() {
         mapView.onDestroy();
     }
 
+    /**
+     * method to initialize the view of this custom view
+     */
     private void init() {
         setLayoutTransition(new LayoutTransition());
         constraintSet = new ConstraintSet();
@@ -128,6 +196,9 @@ public class JRMapView extends ConstraintLayout {
         addClickAction();
     }
 
+    /**
+     * method to add action to all button
+     */
     private void addClickAction() {
         layerButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -167,6 +238,11 @@ public class JRMapView extends ConstraintLayout {
         });
     }
 
+    /**
+     * method to set map type and change selected view on map type chooser
+     *
+     * @param mapType the type code of google map
+     */
     private void setMapType(int mapType) {
         mGoogleMap.setMapType(mapType);
         switch (mapType) {
@@ -197,6 +273,9 @@ public class JRMapView extends ConstraintLayout {
         }
     }
 
+    /**
+     * method to initialize outer space
+     */
     private void initOuterSpace() {
         outerSpace = new ImageView(getContext());
         outerSpace.setId(R.id.outer_space);
@@ -211,12 +290,18 @@ public class JRMapView extends ConstraintLayout {
         outerSpace.setVisibility(GONE);
     }
 
+    /**
+     * method to initialize layer button
+     */
     private void initLayerButton() {
-        layerButton = new ConstraintLayout(getContext());
+        layerButton = new ImageButton(getContext());
         layerButton.setId(R.id.layer_button);
-        layerButton.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.gmwtv_map_type));
+//        layerButton.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.gmwtv_map_type));
+        layerButton.setBackgroundColor(ContextCompat.getColor(getContext(), android.R.color.transparent));
+        layerButton.setImageResource(R.drawable.gmwtv_layer_button);
 
         ViewCompat.setElevation(layerButton, 4);
+
 
         LayoutParams layoutParams = new LayoutParams(
                 LayoutParams.WRAP_CONTENT,
@@ -226,15 +311,6 @@ public class JRMapView extends ConstraintLayout {
         ImageView image = new ImageView(getContext());
         image.setImageResource(R.drawable.gmwtv_layers);
         image.setId(R.id.map_image);
-        layerButton.addView(image, dipToPixel(23), dipToPixel(23));
-
-        ConstraintSet imageConstraintSet = new ConstraintSet();
-        imageConstraintSet.clone(layerButton);
-        imageConstraintSet.connect(R.id.map_image, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
-        imageConstraintSet.connect(R.id.map_image, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
-        imageConstraintSet.connect(R.id.map_image, ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT);
-        imageConstraintSet.connect(R.id.map_image, ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT);
-        imageConstraintSet.applyTo(layerButton);
 
         addView(layerButton, 2, layoutParams);
 
@@ -246,6 +322,9 @@ public class JRMapView extends ConstraintLayout {
         ViewCompat.setElevation(layerButton, 4);
     }
 
+    /**
+     * method to initialize map type chooser view
+     */
     private void initMapType() {
         mapChooser = (ConstraintLayout) LayoutInflater.from(getContext()).inflate(R.layout.gmwtv_layout_layer_chooser, this, false);
 
@@ -268,6 +347,9 @@ public class JRMapView extends ConstraintLayout {
         constraintSet.applyTo(this);
     }
 
+    /**
+     * method to initialize children of map type chooser view
+     */
     private void initMapTypeChildren() {
         defaultMapButton = mapChooser.findViewById(R.id.default_map);
         satelliteMapButton = mapChooser.findViewById(R.id.satellite_map);
@@ -277,11 +359,24 @@ public class JRMapView extends ConstraintLayout {
         terrainText = mapChooser.findViewById(R.id.terrain_text);
     }
 
+    /**
+     * method to convert dip to pixel
+     *
+     * @param dip value in dip
+     * @return value in pixel
+     */
     private int dipToPixel(float dip) {
         return (int) (dip * getContext().getResources().getDisplayMetrics().density + 0.5f);
-//        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
     }
 
+    /**
+     * method to complete padding when googleMap padding changed
+     *
+     * @param left   left padding
+     * @param top    top padding
+     * @param right  right padding
+     * @param bottom bottom padding
+     */
     public void setGoogleMapPadding(int left, int top, int right, int bottom) {
         topPadding = dipToPixel(topPadding) + top;
         mGoogleMap.setPadding(left, top, right, bottom);
@@ -293,11 +388,16 @@ public class JRMapView extends ConstraintLayout {
         constraintSet.applyTo(this);
     }
 
+    /**
+     * method that must run when mapType or myLocationEnabled changed out of this class
+     *
+     * @param googleMap googleMap field from user
+     */
     public void onMapReady(GoogleMap googleMap) {
         this.mGoogleMap = googleMap;
         setMapType(googleMap.getMapType());
 
-        if (googleMap.isMyLocationEnabled()){
+        if (googleMap.isMyLocationEnabled()) {
             topPadding = 56;
             constraintSet.clone(this);
             constraintSet.connect(R.id.layer_button, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, dipToPixel(56));
